9e492440eb2f4d26c1c07edb2a9ae5f134607701
Lucas Augusto <lucasaugustoleon@gmail.com>
2023-09-26 23:49:35 -0300

    refactor: changed svg at new section

--- src/components/ContainerCards.tsx
+++ src/components/ContainerCards.tsx
@@ -1,6 +1,5 @@
 import { motion } from "framer-motion";
-import { Tailwind } from "../../public/assets/tailwind";
-import { reactIcon } from "../../public/assets/reactIcon";
+import { Tailwind } from "../../public/assets/TailwindIcon";
 export const ContainerCards = () => {
   return (
     <motion.ul
@@ -19,42 +18,25 @@ export const ContainerCards = () => {
       gap-4 md:gap-[1.5625rem]
     "
     >
-      <motion.li
-        whileHover={{ scale: 1.1 }}
-        variants={{
-          hidden: { rotate: 70, opacity: 0 },
-          show: { rotate: 0, opacity: 1 },
-        }}
-        className="
-      text-center relative bg-gray-200 shadow-md rounded-3xl transition-all duration-500 flex justify-center items-center group overflow-hidden z-10 w-24 md:w-[8.625rem] aspect-[1/1.1] cursor-pointer
-    "
-      >
-        <reactIcon />
-        <span
+      {
+        <motion.li
+          whileHover={{ scale: 1.1 }}
+          variants={{
+            hidden: { rotate: 70, opacity: 0 },
+            show: { rotate: 0, opacity: 1 },
+          }}
           className="
-            text-zinc-500 cursor-pointer transition-all duration-500 absolute md:-left-10 bottom-4 md:group-hover:left-9 text-md md:text-base font-bold md:opacity-0 md:group-hover:opacity-100 "
+      text-center relative bg-gray-200 shadow-md rounded-3xl transition-all duration-500 flex justify-center items-center group overflow-hidden z-10 w-24 md:w-[8.625rem] aspect-[1/1.1] cursor-pointer"
         >
-          React
-        </span>
-      </motion.li>
-      <motion.li
-        whileHover={{ scale: 1.1 }}
-        variants={{
-          hidden: { rotate: 70, opacity: 0 },
-          show: { rotate: 0, opacity: 1 },
-        }}
-        className="
-      text-center relative bg-gray-200 shadow-md rounded-3xl transition-all duration-500 flex justify-center items-center group overflow-hidden z-10 w-24 md:w-[8.625rem] aspect-[1/1.1] cursor-pointer
-    "
-      >
-        <Tailwind />
-        <span
-          className="
-            text-zinc-500 cursor-pointer transition-all duration-500 absolute md:-left-10 bottom-4 md:group-hover:left-9 text-md md:text-base font-bold md:opacity-0 md:group-hover:opacity-100 "
-        >
-          Tailwind
-        </span>
-      </motion.li>
+          <Tailwind />
+          <span
+            className="
+            text-zinc-500 cursor-pointer transition-all duration-500 absolute md:-left-10 bottom-4 md:group-hover:left-9 text-md md:text-base font-bold md:opacity-0 md:group-hover:opacity-100"
+          >
+            Tailwind
+          </span>
+        </motion.li>
+      }
     </motion.ul>
   );
 };
