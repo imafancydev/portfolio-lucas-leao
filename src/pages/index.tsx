0b549d2d864a29e74afbe01d3389b5344579dca8
Lucas Augusto <lucasaugustoleon@gmail.com>
2023-10-02 15:17:55 -0300

    SEO: SEO project config

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -7,6 +7,48 @@ import Head from "next/head";
 const Home = () => {
   return (
     <>
+      <Head>
+        <title>Lucas Leão | Desenvolvedor de software</title>
+        <link rel="icon" type="image/x-icon" href="/assets/logo.svg" />
+        <meta name="title" content="Lucas Leão" />
+        <meta name="language" content="pt-br" />
+        <meta
+          name="description"
+          content="Lucas Leão e um jovem programador Full-Stack que busca fazer o melhor software para suprir a demanda do mercado"
+        />
+        <meta
+          name="keywords"
+          content="Lucas Leão dev, Lucas Leão front-end, Lucas Leão, Leão, Lucas, Lucas front-end Leão"
+        />
+        <meta name="robots" content="index, follow" />
+
+        <meta property="og:type" content="website" />
+        <meta property="og:url" content="https://lucasleaodev.vercel.app/" />
+        <meta property="og:title" content="Lucas Leão" />
+        <meta
+          property="og:description"
+          content="Lucas Leão e um jovem programador Full-Stack que busca fazer o melhor software para suprir a demanda do mercado."
+        />
+        <meta
+          property="og:image"
+          content="https://st5.depositphotos.com/14021298/65051/i/450/depositphotos_650516380-stock-photo-pink-paint-pastel-color-background.jpg"
+        />
+
+        <meta property="twitter:card" content="summary_large_image" />
+        <meta
+          property="twitter:url"
+          content="https://lucasleaodev.vercel.app/"
+        />
+        <meta property="twitter:title" content="Lucas Leão" />
+        <meta
+          property="twitter:description"
+          content="Lucas Leão e um jovem programador Full-Stack que busca fazer o melhor software para suprir a demanda do mercado."
+        />
+        <meta
+          property="twitter:image"
+          content="https://st5.depositphotos.com/14021298/65051/i/450/depositphotos_650516380-stock-photo-pink-paint-pastel-color-background.jpg"
+        />
+      </Head>
       <div className="bg-gray-200">
         <LayoutsPageDefault>
           <MainSection />
