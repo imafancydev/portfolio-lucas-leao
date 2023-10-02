b6f367376a1a6b80415a5b527a1b316fe346fca3
Lucas Augusto <lucasaugustoleon@gmail.com>
2023-10-02 15:20:40 -0300

    fix: fixed main section

--- src/screens/Home/MainSection.tsx
+++ src/screens/Home/MainSection.tsx
@@ -19,7 +19,7 @@ export const MainSection = () => {
         <p className="text-zinc-900 text-2xl">
           Desenvolvedor{" "}
           <span className="font-bold text-4xl text-transparent bg-clip-text @apply bg-[#CA4246] bg-[linear-gradient(_45deg,#CA4246_16.666%,#E16541_16.666%,#E16541_33.333%,#F18F43_33.333%,#F18F43_50%,#8B9862_50%,#8B9862_66.666%,#476098_66.666%,#476098_83.333%,#A7489B_83.333%)] bg-[100%] bg-repeat animate-[rainbow-text-simple-animation-rev_0.75s_ease_forwards] hover:animate-[rainbow-text-simple-animation_0.5s_ease-in_forwards] cursor-pointer">
-            front-end
+            Full-Stack
           </span>
         </p>
 
